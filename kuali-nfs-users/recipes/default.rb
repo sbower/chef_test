24107fedc9104c29b8f87e3f2cd8f860f5f9da45
sbower <shawn.bower@gmail.com>
2016-01-23 20:49:39 -0500

    fixing up the users

--- kuali-nfs-users/recipes/default.rb
+++ kuali-nfs-users/recipes/default.rb
@@ -16,7 +16,7 @@ end
 user 'tcowner' do
   comment 'tcowner account to scyn with on prem'
   uid '11448'
-  gid '7070'
+  gid 'tcadm'
   home '/home/tcowner'
   shell '/bin/bash'
 end
@@ -24,7 +24,7 @@ end
 user 'tomcat7' do
   comment 'tcowner account to scyn with docker'
   uid '105'
-  gid '7070'
+  gid 'tcadm'
   home '/home/tomcat7'
   shell '/bin/bash'
 end
